625d1775ad286f5b700c1fd9c42367b4dd4d5893
Michal Petrik <misopetrik@gmail.com>
2020-10-31 13:44:31 +0100

    :truck: Add routes

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('API is up and running')
-})
+// Connect DB
+connectDB();
+
+app.get('/', (req, res) => res.send('API is up and running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => `Server started at ${PORT}`);
