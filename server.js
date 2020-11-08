6d5efe4faffe874f024dc16a6065115f9176103c
Michal Petrik <misopetrik@gmail.com>
2020-11-08 11:22:29 +0100

    :construction: Add Navbar, Register and Login components

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// Connect DB
 connectDB();
 
-// Init Middleware
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API is up and running'));
 
-// Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
