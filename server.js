744a85d7a09e89e29b23bbea0828b6a0ffc593fd
Michal Petrik <misopetrik@gmail.com>
2020-12-19 19:05:34 +0100

    :poop: Another emoji test

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-console.log(🩹, 🎉)
+
 app.listen(PORT, () => `Server started at ${PORT}`);
